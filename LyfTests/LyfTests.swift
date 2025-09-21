45179a7681ca6ea606fd0e9fff5b4911dc5e5765
pauloo07 <santoozdev@gmail.com>
2025-09-20 21:13:25 -0300

    Initial Commit

---
+++ LyfTests/LyfTests.swift
@@ -0,0 +1,17 @@
+//
+//  LyfTests.swift
+//  LyfTests
+//
+//  Created by paaaulo santos on 20/09/25.
+//
+
+import Testing
+@testable import Lyf
+
+struct LyfTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
